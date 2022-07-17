4dfa13e7da45d4a5009e4442ec1ff3ad09009fce
Duncan Oluoch <oluochdancun19@gmail.com>
2022-07-17 17:59:51 +0300

    shopster

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React,{useState}from "react";
 
 function Item({ name, category }) {
+  const[incart, setCart]=useState(false)
+
+  function addToCart(){
+    setCart((incart)=>!incart)
+  }
+  const cartToggle =incart ? "Remove from cart": "Add to cart"
+  const listClass= incart ? "in-cart" :"remove"
   return (
-    <li className="">
+    <li className={incart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+        <button className={incart ? "in-cart" : "remove"} onClick={addToCart}>   {incart ? "Remove From" : "Add to"} Cart</button>
     </li>
   );
 }
